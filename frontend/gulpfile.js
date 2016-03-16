2adc7907ae32d8e1d38280af2415cd612cd8f92a
Tom Najdek <tom@doppnet.com>
2016-03-16 16:44:50 +0000

    Production ready builds + cleanup

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -13,57 +13,77 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssminify = require('gulp-minify-css');
+const envify = require('gulp-envify');
+const merge = require('merge-stream');
+const rimraf = require('rimraf');
 
 function onError(err) {
 	console.warn(err);
 }
 
 function getBuild(dev) {
+	process.env.NODE_ENV = dev ? 'development' : 'production';
+
 	var b = browserify({
 		debug: true,
-		entries: './zsr.js',
+		entries: './src/zsr.js',
 		standalone: "ZSR",
+		globalTransform: true,
 		transform: [
-        	['babelify', {
-        		'presets': ['es2015']
-    		}]
+			['babelify', {
+				'presets': ['es2015']
+			}],
 		]
 	});
 
 	return b.bundle()
-		.pipe(source('./zsr.js'))
+		.pipe(source('zsr.js'))
 		.pipe(buffer())
 		.pipe(plumber({errorHandler: onError}))
-		.pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./build/'))
-		.pipe(filter('*.js'))
-		.pipe(uglify())
-		.pipe(rename({ extname: '.min.js' }))
-		.pipe(gulp.dest('build'));
+		.pipe(gulpif(dev, sourcemaps.init({loadMaps: true})))
+		.pipe(envify())
+		.pipe(filter('**/*.js'))
+		.pipe(gulpif(!dev, uglify()))
+		.pipe(gulpif(dev, sourcemaps.write('./')))
+		.pipe(gulp.dest('./build/js/'))
 }
 
-gulp.task('default', function() {
-	return getBuild(false);
-});
+function getSass(dev) {
+	return gulp.src('./scss/*.scss')
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(gulpif(dev, sourcemaps.init({loadMaps: true})))
+		.pipe(sass())
+		.pipe(autoprefixer({
+				browsers: ['last 2 versions']
+		}))
+		.pipe(filter('**/*.css'))
+		.pipe(gulpif(!dev, cssminify()))
+		.pipe(gulpif(dev, sourcemaps.write('./')))
+		.pipe(gulp.dest('build/css/'));
+}
 
-gulp.task('jsDev', function() {
-	return getBuild(true);
+gulp.task('clean', function(done) {
+	rimraf('./build', done);
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
-    .pipe(plumber({errorHandler: onError}))
-    .pipe(sass())
-    .pipe(gulp.dest('./build'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/*.scss', ['sass']);
+  return getSass(true);
 });
 
-
-gulp.task('dev', ['sass', 'sass:watch'], function() {
-	gulp.watch('./zsr.js', ['jsDev']);
-	return getBuild(true);
+gulp.task('js', function () {
+  return getBuild(true);
 });
+
+gulp.task('dev', ['clean'], function() {
+	gulp.watch('./src/zsr.js', ['js']);
+	gulp.watch('./scss/*.scss', ['sass']);
+	return merge(getSass(true), getBuild(true));
+});
+
+gulp.task('build', ['clean'], function() {
+	return merge(getSass(false), getBuild(false));
+});
+
+gulp.task('default', ['dev']);
